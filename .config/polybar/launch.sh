ae84cb4bbd22afd49c5f5394768a5e36baf87097
ZodiacalComet <zodiacalcomet@gmail.com>
2020-05-13 00:44:32 -0400

    Change polybar theme to one made by myself
    Simple theme with nord colors and only the modules that I need.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini top &
-polybar -c ~/.config/polybar/config.ini bottom &
+# Launch top and bottom bar
+polybar -c ~/.config/polybar/config.ini topbar &
+polybar -c ~/.config/polybar/config.ini bottombar &
 
