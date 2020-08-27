830acc9e4d0fed4314f64d51b46eabf400d70a87
ZodiacalComet <ZodiacalComet@gmail.com>
2020-08-26 20:38:01 -0400

    Fix play-url path in qutebrowser bind

--- .config/qutebrowser/qute-binds.py
+++ .config/qutebrowser/qute-binds.py
@@ -6,5 +6,5 @@ config = config  # noqa: F821
 config.bind("<Ctrl-r>", "config-source")
 
 # Spawn mpv on selected url with hint
-play_url_script = str(Path.home() / ".scripts" / "play-url")
+play_url_script = str(Path.home() / ".scripts" / "bin" / "play-url")
 config.bind("<Ctrl-p>", "hint links spawn --detach " + play_url_script + " {hint-url}")
