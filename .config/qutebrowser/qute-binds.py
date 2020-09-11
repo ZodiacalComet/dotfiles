8eaeafeff12d8118523cbcd43ed9e50dc2772dd4
ZodiacalComet <ZodiacalComet@gmail.com>
2020-09-10 22:43:38 -0400

    Add show-yt-info personal script
    qutebrowser doesn't load the comments from Youtube, so I made an script
    to allow me to read them without opening the same page on another
    browser.
    
    Said script has a function taken from [Rapptz/discord.py](https://github.com/Rapptz/discord.py).

--- .config/qutebrowser/qute-binds.py
+++ .config/qutebrowser/qute-binds.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from os import getenv
 
 config = config  # noqa: F821
 
@@ -8,3 +9,8 @@ config.bind("<Ctrl-r>", "config-source")
 # Spawn mpv on selected url with hint
 play_url_script = str(Path.home() / ".scripts" / "bin" / "play-url")
 config.bind("<Ctrl-p>", "hint links spawn --detach " + play_url_script + " {hint-url}")
+
+# Spawn show-yt-info script on TERMINAL
+TERM = getenv("TERMINAL", "st")
+show_yt_info_script = str(Path.home() / ".scripts" / "show-yt-info")
+config.bind("<Ctrl-i>", f"hint links spawn --detach {TERM} {show_yt_info_script}" + " {hint-url}")
